3579fb5781982a70b8e31e5250af07174e0d8e74
Colin Osborn <colin.osborn@gmail.com>
2016-07-19 10:31:24 -0600

    adding more tests

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -23,5 +23,27 @@ class Api::V1::ItemsControllerTest < ActionDispatch::IntegrationTest
     assert_equal item.name, parsed_item["name"]
     assert_equal item.description, parsed_item["description"]
   end
+  test "can create a new item" do
+    item_params = {name: "Saw", description: "I want to play a game"}
+    post "/api/v1/items.json", params: {item: item_params}
+
+    assert_response :success
+
+    parsed_item = JSON.parse(response.body)
+    item = Item.last
+
+    assert_equal item.id, parsed_item["id"]
+    assert_equal item.name, parsed_item["name"]
+    assert_equal item.description, parsed_item["description"]
+  end
+  test "can update an item" do
+    original_item = items(:two)
+    item_params = {name: "Hack Saw"}
+    patch "/api/v1/items/#{original_item.id}", params: {item: item_params}
+
+    assert_response :success
+    updated_item = JSON.parse(response.body)
+    assert_equal item_params[:name], updated_item["name"]
+  end
 
 end
